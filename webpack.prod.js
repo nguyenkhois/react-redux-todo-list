db292a9212fe894a0c6dd4e426960212eff70f9d
Khoi Le <minarek@outlook.com>
2018-08-08 18:56:38 +0200

    Added optimization to deployment

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,29 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = merge(common, {
     mode: 'production',
     plugins: [
         new webpack.DefinePlugin({'process.env.NODE_ENV':JSON.stringify('production')})
-    ]
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new UglifyJsPlugin({
+                parallel: true,
+                sourceMap: true,
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,                
+                uglifyOptions: {
+                    compress: true,
+                    arrows: true,
+                    output: {
+                        comments: false
+                    }
+                }
+            })
+        ]
+    }
 });
