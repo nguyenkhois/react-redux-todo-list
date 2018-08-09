7dbff23c43cc9850fa65671e369b27ea63f823a7
Khoi Le <minarek@outlook.com>
2018-08-09 22:22:19 +0200

    Updated packages

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const customConfigs = require('./webpack.custom'); // Using your own configs
 
 module.exports = {
-    entry: './src/index.jsx',// which file to begin with, 
+    entry: './src/index.js',// which file to begin with, 
     output: {
         path: path.resolve(__dirname, customConfigs.distDir), // what folder to put bundle in
         filename: '[name].[hash].js' // what name to use for bundle
